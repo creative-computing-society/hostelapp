e7847058eaf96adfcd28eba1629f0576659e3720
kush-daga <kushdaga@hotmail.com>
2019-03-27 17:46:25 +0530

    made changes

--- App.js
+++ App.js
@@ -1,13 +1,12 @@
 import React from "react";
-import { StyleSheet, Text, View } from "react-native";
-
+import { StyleSheet, Text, View, ScrollView } from "react-native";
+import MainPage from "./components/MainPage";
 export default class App extends React.Component {
     render() {
         return (
-            <View style={styles.container}>
-                <Text style={{ color: "blue" }}>HELLO!</Text>
-                <Text style={{ color: "red" }}>HELLO!</Text>
-            </View>
+            <ScrollView style={styles.container}>
+                <MainPage />
+            </ScrollView>
         );
     }
 }
@@ -15,8 +14,6 @@ export default class App extends React.Component {
 const styles = StyleSheet.create({
     container: {
         flex: 1,
-        backgroundColor: "#000",
-        alignItems: "center",
-        justifyContent: "center"
+        backgroundColor: "#fff"
     }
 });
