d836735e7a27979c5d4ea5f0cf70ce106180dbb1
MankaranSingh <42907308+MankaranSingh@users.noreply.github.com>
2019-04-05 16:40:09 +0530

    fixed bugs + added fetching features (not tested yet)

--- api_v1.0/flask_server - Copy.py
+++ api_v1.0/flask_server - Copy.py
@@ -16,21 +16,6 @@ db = SQLAlchemy(app)
 users = []
 
 #Databases
-
-class Hostel(db.Model):
-
-    __tablename__ = 'hostels'
-
-    id = db.Column(db.Integer, primary_key = True, nullable = False)
-    name = db.Column(db.String(10), nullable = False)
-    warden = db.Column(db.String(50), nullable = True)
-    caretaker = db.Column(db.String(50), nullable = True)
-    mess_members = db.Column(db.String(50), nullable = True)  # format - 'member1:member2:member3'
-    student_representatives = db.Column(db.String(50), nullable = True)  # format - 'student1:student2:student3'
-    mess_menus = db.relationship('Mess_Menu', backref = 'hostel', lazy ='dynamic')
-    
-
-    
 class User(db.Model):
 
     __tablename__ = 'users'
@@ -44,16 +29,15 @@ class User(db.Model):
     hostel = db.Column(db.String(10), nullable = False)
     complaints = db.relationship('Complaint', backref = 'student', lazy ='dynamic')
 
-class Mess_Menu(db.Model):
+class Mess_Menu_Week1(db.Model):
 
-    __tablename__ = 'mess_menus'
+    __tablename__ = 'mess_menu'
 
     '''
-        Format - Timings::BreakFast::Timings::Lunch::Timings::Dinner
+        Format - Hostel:Timings:BreakFast:Timings:Lunch:Timings:Dinner
     '''
     id = db.Column(db.Integer, primary_key = True, nullable = False)
-    week = db.Column(db.Integer, nullable=False)
-    hostel = db.Column(db.String(10), nullable=False)
+    hostel = db.db.Column(db.String(10), nullable=False)
     sunday = db.Column(db.String(500), nullable=False)
     monday = db.Column(db.String(500), nullable=False)
     tuesday = db.Column(db.String(500), nullable=False)
@@ -61,7 +45,23 @@ class Mess_Menu(db.Model):
     thursday = db.Column(db.String(500), nullable=False)
     friday = db.Column(db.String(500), nullable=False)
     saturday = db.Column(db.String(500), nullable=False)
-    hostel_id = db.Column(db.Integer, db.ForeignKey('mess_menus.id'))
+
+class Mess_Menu_Week2(db.Model):
+
+    __tablename__ = 'mess_menu'
+
+    '''
+        Format - Hostel:Timings:BreakFast:Timings:Lunch:Timings:Dinner
+    '''
+    id = db.Column(db.Integer, primary_key = True, nullable = False)
+    hostel = db.db.Column(db.String(10), nullable=False)
+    sunday = db.Column(db.String(500), nullable=False)
+    monday = db.Column(db.String(500), nullable=False)
+    tuesday = db.Column(db.String(500), nullable=False)
+    wednesdaay = db.Column(db.String(500), nullable=False)
+    thursday = db.Column(db.String(500), nullable=False)
+    friday = db.Column(db.String(500), nullable=False)
+    saturday = db.Column(db.String(500), nullable=False)
 
 class Complaint(db.Model):
 
@@ -73,8 +73,7 @@ class Complaint(db.Model):
     description = db.Column(db.Text(), nullable = False)
     timestamp = db.Column(db.DateTime(),default = datetime.utcnow)
     student_id = db.Column(db.Integer, db.ForeignKey('users.id'))
-    hostel = db.Column(db.String(10), nullable = False)
-    room_number = db.Column(db.String(10), nullable = False)
+    hostel = db.Column(db.String(10), nullable = False) 
 
 
 class Utility():
@@ -147,7 +146,7 @@ def login():
 
     '''
         format - {'email':str, 'password':str}
-         
+        return_format - {}
     '''
     
     if request.method == 'POST':
@@ -182,90 +181,20 @@ def register_complaint():
             return jsonify(status='503')
 
 
+
 @app.route('/v1.0/log_check',methods=['GET','POST'])
 def login_check():
     credentials = request.get_json()
     return jsonify(status=Utility.is_logged_in(credentials['roll_number']))
 
-
 @app.route('/v1.0/fetch_complaints',methods=['GET','POST'])
 def fetch_complaints():
+    pass
 
-    if request.method == 'POST':
-        json = request.get_json()
-        complaints = Complaint.query.filter_by(hostel = json['hostel']).all()
-         
-
-@app.route('/v1.0/fetch_menu',methods=['GET','POST'])  # Work with caution, mindfux ahead
+@app.route('/v1.0/fetch_menu',methods=['GET','POST'])
 def fetch_mess_menu():
+    pass
 
-    '''
-        input format - {hostel:string, week:int}
-        return_format - {
-                             header:{hostel:str,week:int}
-                             food:{weekdays:str}
-                             timings:{weekdays:str}
-                         }  
-    '''
-    
-    if request.method == 'POST':
-        
-        json = request.get_json()
-        menu = Mess_Menu.query.filter_by(hostel=json['hostel'], week=int(json['week'])).first()
-        if menu:
-            return jsonoify(
-                            {
-                                header:{hostel:json['hostel'], week:json['week']},\
-                                        
-                                food:{
-                                    
-                                    {breakfast:{sunday:menu.sunday.split('::')[1], monday:menu.monday.split('::')[1],tuesday:menu.tuesday.split('::')[1],wednesday:menu.wednesday.split('::')[1],thursday:menu.thursday.split('::')[1],friday:menu.friday.split('::')[1],saturday:menu.saturday.split('::')[1]}},\
-                                    {lunch:{sunday:menu.sunday.split('::')[3], monday:menu.monday.split('::')[3],tuesday:menu.tuesday.split('::')[3],wednesday:menu.wednesday.split('::')[3],thursday:menu.thursday.split('::')[3],friday:menu.friday.split('::')[3],saturday:menu.saturday.split('::')[3]}},\
-                                    {dinner:{sunday:menu.sunday.split('::')[5], monday:menu.monday.split('::')[5],tuesday:menu.tuesday.split('::')[5],wednesday:menu.wednesday.split('::')[5],thursday:menu.thursday.split('::')[5],friday:menu.friday.split('::')[5],saturday:menu.saturday.split('::')[5]}},\
-
-                                    },
-                                timings:{
-                                    
-                                    {breakfast:{sunday:menu.sunday.split('::')[0], monday:menu.monday.split('::')[0],tuesday:menu.tuesday.split('::')[0],wednesday:menu.wednesday.split('::')[0],thursday:menu.thursday.split('::')[0],friday:menu.friday.split('::')[0],saturday:menu.saturday.split('::')[0]}},\
-                                    {lunch:{sunday:menu.sunday.split('::')[2], monday:menu.monday.split('::')[2],tuesday:menu.tuesday.split('::')[2],wednesday:menu.wednesday.split('::')[2],thursday:menu.thursday.split('::')[2],friday:menu.friday.split('::')[2],saturday:menu.saturday.split('::')[2]}},\
-                                    {dinner:{sunday:menu.sunday.split('::')[4], monday:menu.monday.split('::')[4],tuesday:menu.tuesday.split('::')[4],wednesday:menu.wednesday.split('::')[4],thursday:menu.thursday.split('::')[4],friday:menu.friday.split('::')[4],saturday:menu.saturday.split('::')[4]}},\
-                             }
-                            }
-                            )
-        return jsonify(error='menu not found')
-
-
-@app.route('/v1.0/update_menu',methods=['GET','POST'])   # if menu is not created yet, create else updating takes place.
-def update_menu():
-
-    if request.method == 'POST':
-        menu_details = request.get_json()
-
-        menu = Mess_Menu.query.filter_by(hostel=menu_details['hostel'],week=int(menu_details['week']))
-
-        if menu is None:
-        
-            try:
-                menu = Mess_Menu(hostel=menu_details['hostel'], week=int(menu_details['week']), sunday=menu_details['sunday'],monday=menu_details['monday'],tuesday=menu_details['tuesday']\
-                                 ,wednesday=menu_details['wednesday'],thursday=menu_details['thursday'],friday=menu_details['friday'],saturday=menu_details['saturday'])
-                db.session.add(menu)
-                db.session.commit()
-
-                return {message:'menu updated successfully'}
-            except:
-                return jsonify(error='bad request')
-        else:
-            menu.sunday = menu_details['sunday']
-            menu.monday = menu_details['monday']
-            menu.tuesday = menu_details['tuesday']
-            menu.wednesday = menu_details['wednesday']
-            menu.thursday = menu_details['thursday']
-            menu.friday = menu_details['friday']
-            menu.saturday = menu_details['saturday']
-
-            db.session.commit()
-            
-            
 db.create_all()
 
 if __name__ == '__main__':
