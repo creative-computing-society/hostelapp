9c729b29fd52eca859c78887c129568f981eae43
MankaranSingh <42907308+MankaranSingh@users.noreply.github.com>
2019-04-15 18:55:41 +0530

    Database uspdated + added secure authentication

--- api_v1.0/flask_server.py
+++ api_v1.0/flask_server.py
@@ -26,59 +26,117 @@ class Hostel(db.Model):
 
     id = db.Column(db.Integer, primary_key = True, nullable = False)
     name = db.Column(db.String(10), nullable = False)
-    warden = db.Column(db.String(50), nullable = True)
-    caretaker = db.Column(db.String(50), nullable = True)
-    mess_members = db.Column(db.String(50), nullable = True)  # format - 'member1:member2:member3'
-    student_representatives = db.Column(db.String(50), nullable = True)  # format - 'student1:student2:student3'
     mess_menus = db.relationship('Mess_Menu', backref = 'hostel', lazy ='dynamic')
     complaints = db.relationship('Complaint', backref = 'hostel', lazy ='dynamic')
-
-
+    members = db.relationship('User' , backref = 'hostel', lazy = 'dynamic' )
+    notifications = db.relationship('Notification', backref = 'hostel', lazy ='dynamic')
+    
 class User(db.Model):
 
     __tablename__ = 'users'
 
     id = db.Column(db.Integer, primary_key = True, nullable = False)
+    role_id = db.Column(db.Integer, db.ForeignKey('roles.id'))
     username = db.Column(db.String(50), nullable = False)
     roll_number = db.Column(db.Integer(), unique= True, nullable = False)
     email = db.Column(db.String(120), unique= True, nullable = False)
     password = db.Column(db.String(120), nullable = False)
     phone_number = db.Column(db.Integer(), unique= True, nullable = False)
-    hostel = db.Column(db.String(10), nullable = False)
+    hostel_id = db.Column(db.Integer, db.ForeignKey('hostels.id'))
     complaints = db.relationship('Complaint', backref = 'student', lazy ='dynamic')
+    leave_entries = db.relationship('LeaveEntries' , backref = 'student', lazy = 'dynamic' )
+    notifications = db.relationship('Notification' , backref = 'user', lazy = 'dynamic' )
 
 class Mess_Menu(db.Model):
 
     __tablename__ = 'mess_menus'
 
-    '''
-        Format - Timings::BreakFast::Timings::Lunch::Timings::Dinner
-    '''
     id = db.Column(db.Integer, primary_key = True, nullable = False)
-    week = db.Column(db.Integer, nullable=False)
-    hostel_name = db.Column(db.String(10), nullable=False)
-    sunday = db.Column(db.String(500), nullable=False)
-    monday = db.Column(db.String(500), nullable=False)
-    tuesday = db.Column(db.String(500), nullable=False)
-    wednesday = db.Column(db.String(500), nullable=False)
-    thursday = db.Column(db.String(500), nullable=False)
-    friday = db.Column(db.String(500), nullable=False)
-    saturday = db.Column(db.String(500), nullable=False)
     hostel_id = db.Column(db.Integer, db.ForeignKey('hostels.id'))
+    week = db.Column(db.Integer, nullable=False)    # 1,2,3
+    day = db.Column(db.String(50), nullable=False)  # Sunday, Monday, etc.
+    category = db.Column(db.String(50), nullable=False)  # Breakfast, Lunch, Dinner
+    info = db.Column(db.Text(), nullable = True)        # Any special stuff  (Optional)
+    meal = db.Column(db.Text(), nullable = False)       # Actual menu here 
+    timings =db.Column(db.String(20), nullable = False)  # format = 8:00 AM-10:00 AM 
+    
+class ComplaintCategory(db.Model):
 
+    __tablename__ = 'complaint_categories'
+    
+    id = db.Column(db.Integer, primary_key = True, nullable = False)
+    name = db.Column(db.String(120), nullable = False)
+    complaints = db.relationship('Complaint' , backref = 'category', lazy = 'dynamic')
+    
 class Complaint(db.Model):
 
     __tablename__ = 'complaints'
 
     id = db.Column(db.Integer, primary_key = True, nullable = False)
-
-    category = db.Column(db.String(120), nullable = False)
+    
+    category_id = db.Column(db.Integer, db.ForeignKey('complaint_categories.id'))
+    student_id = db.Column(db.Integer, db.ForeignKey('users.id'))
+    hostel_id = db.Column(db.Integer, db.ForeignKey('hostels.id'))
+    
     description = db.Column(db.Text(), nullable = False)
     timestamp = db.Column(db.DateTime(),default = datetime.utcnow)
-    student_id = db.Column(db.Integer, db.ForeignKey('users.id'))
     hostel_name = db.Column(db.String(10), nullable = False)
     room_number = db.Column(db.String(10), nullable = False)
+    
+    completed = db.Column(db.Boolean)
+    
+
+class Role(db.Model):
+
+    __tablename__ = 'roles'
+
+    id = db.Column(db.Integer, primary_key = True)
+    role_name =  db.Column(db.String(50), unique = True)
+    users = db.relationship('User' , backref = 'role', lazy = 'dynamic' )
+    permissions = db.Column(db.Integer)
+
+    @staticmethod
+    def insert_roles():
+
+        roles = [
+                 'user',
+                 'warden/caretaker',
+                 'mess_members',
+                 'god',
+                 'moderators',
+                 'guard'
+                ]
+
+        for role in roles:
+            if Role.query.filter_by(role_name = str(r)).first() is None:
+                role = Role(role_name = str(role))
+                db.session.add(role)
+            else:
+                continue
+        db.session.commit()
+
+class Notification(db.Model):
+
+    __tablename__ = 'notifications'
+
+    id = db.Column(db.Integer, primary_key = True)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
     hostel_id = db.Column(db.Integer, db.ForeignKey('hostels.id'))
+    
+    start_date = db.Column(db.DateTime())
+    end_date = db.Column(db.DateTime())
+    description = db.Column(db.Text(), nullable = False)
+
+class LeaveEntries(db.Model):
+
+    __tablename__ = 'leave_entries'
+
+    id = db.Column(db.Integer, primary_key = True)
+    student_id = db.Column(db.Integer, db.ForeignKey('users.id'))
+    leave_time = db.Column(db.DateTime(),default = datetime.utcnow)
+    leaving_place = db.Column(db.String(50))
+    in_time = db.Column(db.DateTime())
+    verified = db.Column(db.Boolean)
 
 class Utility():
 
@@ -228,81 +286,17 @@ def fetch_complaints():
 @app.route('/v1.0/fetch_menu',methods=['GET','POST'])  # Work with caution, mindfux ahead
 def fetch_mess_menu():
 
-    '''
-        input format - {hostel:string, week:int}
-        return_format - {
-                             header:{hostel:str,week:int}
-                             food:{weekdays:str}
-                             timings:{weekdays:str}
-                         }  
-    '''
-    
-    if request.method == 'POST':
-        
-        json = request.get_json()
-        menu = Mess_Menu.query.filter_by(hostel_name=json['hostel'], week=int(json['week'])).first()
-        if menu:
-            
-            return jsonify(
-                            {
-                                "header":{"hostel":json['hostel'], "week":json['week']},\
-                                        
-                                "food":{
-                                    
-                                    'breakfast':{'sunday':menu.sunday.split('::')[1], 'monday':menu.monday.split('::')[1],'tuesday':menu.tuesday.split('::')[1],'wednesday':menu.wednesday.split('::')[1],'thursday':menu.thursday.split('::')[1],'friday':menu.friday.split('::')[1],'saturday':menu.saturday.split('::')[1]},\
-                                    'lunch':{'sunday':menu.sunday.split('::')[3], 'monday':menu.monday.split('::')[3],'tuesday':menu.tuesday.split('::')[3],'wednesday':menu.wednesday.split('::')[3],'thursday':menu.thursday.split('::')[3],'friday':menu.friday.split('::')[3],'saturday':menu.saturday.split('::')[3]},\
-                                    'dinner':{'sunday':menu.sunday.split('::')[5], 'monday':menu.monday.split('::')[5],'tuesday':menu.tuesday.split('::')[5],'wednesday':menu.wednesday.split('::')[5],'thursday':menu.thursday.split('::')[5],'friday':menu.friday.split('::')[5],'saturday':menu.saturday.split('::')[5]}
-
-                                    },
-                                "timings":{
-                                    
-                                    'breakfast':{'sunday':menu.sunday.split('::')[0], 'monday':menu.monday.split('::')[0],'tuesday':menu.tuesday.split('::')[0],'wednesday':menu.wednesday.split('::')[0],'thursday':menu.thursday.split('::')[0],'friday':menu.friday.split('::')[0],'saturday':menu.saturday.split('::')[0]},\
-                                    'lunch':{'sunday':menu.sunday.split('::')[2], 'monday':menu.monday.split('::')[2],'tuesday':menu.tuesday.split('::')[2],'wednesday':menu.wednesday.split('::')[2],'thursday':menu.thursday.split('::')[2],'friday':menu.friday.split('::')[2],'saturday':menu.saturday.split('::')[2]},\
-                                    'dinner':{'sunday':menu.sunday.split('::')[4], 'monday':menu.monday.split('::')[4],'tuesday':menu.tuesday.split('::')[4],'wednesday':menu.wednesday.split('::')[4],'thursday':menu.thursday.split('::')[4],'friday':menu.friday.split('::')[4],'saturday':menu.saturday.split('::')[4]}
-                             }
-                            }
-                            )
-        return jsonify(error='menu not found')
+    pass
 
 
 @app.route('/v1.0/update_menu',methods=['GET','POST'])   # if menu is not created yet, create else updating takes place.
 def update_menu():
 
-    if request.method == 'POST':
-        menu_details = request.get_json()
-
-        menu = Mess_Menu.query.filter_by(hostel_name=menu_details['hostel'],week=int(menu_details['week'])).first()
-        print(menu)
-
-        if menu is None:
-        
-            
-            menu = Mess_Menu(hostel_name=menu_details['hostel'], week=int(menu_details['week']), sunday=menu_details['sunday'],monday=menu_details['monday'],tuesday=menu_details['tuesday']\
-                             ,wednesday=menu_details['wednesday'],thursday=menu_details['thursday'],friday=menu_details['friday'],saturday=menu_details['saturday'])
-            hostel_object = Hostel.query.filter_by(name=menu_details['hostel'])
-            
-            db.session.add(menu)
-            db.session.commit()
-
-            return jsonify(message='menu created successfully')
-            
-        else:
-            menu.sunday = menu_details['sunday']
-            menu.monday = menu_details['monday']
-            menu.tuesday = menu_details['tuesday']
-            menu.wednesday = menu_details['wednesday']
-            menu.thursday = menu_details['thursday']
-            menu.friday = menu_details['friday']
-            menu.saturday = menu_details['saturday']
-
-            db.session.commit()
-            return jsonify(message='menu updated successfully')
+    pass
 
 db.drop_all()
 db.create_all()
 
-Utility.create_hostels('H','ABC','ABC', 'Mankaran Singh::Udhbav Oberoi', 'Mankaran Singh::Udhbav Oberoi::Nikhil')    # name, warden, caretaker, mess_members,student_representatives
-Utility.create_hostels('B','ABC','ABC', 'Mankaran Singh::Udhbav Oberoi', 'Mankaran Singh::Udhbav Oberoi::Nikhil')
 
 if __name__ == '__main__':
     app.run(debug=True)
